ecf65961e6fd644e034aaf944f2f793710d33347
Henry Moland <molandh@gmail.com>
2019-05-22 19:11:12 -0700

    Added form input for name, email and message

--- src/components/Home.js
+++ src/components/Home.js
@@ -100,21 +100,17 @@ const Home = () => {
              
             </section>
 
+            
             <section id="projects">
                 <div className="container">
                     <div className="project-wrapper">
                         <h3 className="section-heading">My projects</h3>
 
                         <div className="box" style={{background: "url(https://assets.wordpress.envato-static.com/uploads/2017/02/01-diagonal-slanted-web-design.jpg) no-repeat center center", backgroundSize: "cover"}}>
-                        </div>
-
-
-                        <div className="box" style={{background: "url(https://leerob.io/static/images/how-stripe-designs-beautiful-websites/website.png) no-repeat center center", backgroundSize: "cover"}}>
-                            <div className="hover-box">
-                                <h3 className="project-box-heading">Project 2</h3>
-                                <p style={{fontSize: ".8rem", fontWeight: 400}}>Project 2 is an application created to allow users send message. It was created with react</p>
-                                
-                                <p style={{fontWeight: 700, textAlign: "center", marginTop: 5}}>Tech Stack:</p>
+                        <div className="hover-box">
+                                <h3 className="project-box-heading">Pay Me</h3>
+                                <p>Project 2 is an application created to allow users send message. It was created with react</p>
+                                <h6>Tech Stack:</h6>
                                 <div className="row">
                                     <div className ="col">
                                         <p style={{fontSize: ".8rem", fontWeight: 400}}>React</p>
@@ -125,22 +121,147 @@ const Home = () => {
                                         <p style={{fontSize: ".8rem", fontWeight: 400}} >Firebase</p>
                                     </div>
                                 </div>
+                                <button className="btn btn-light" style={{marginTop: 15}}>View Project</button>
+                            </div>
+                        </div>
 
+
+                        <div className="box" style={{background: "url(https://leerob.io/static/images/how-stripe-designs-beautiful-websites/website.png) no-repeat center center", backgroundSize: "cover"}}>
+                            <div className="hover-box">
+                                <h3 className="project-box-heading">Pay Me</h3>
+                                <p>Project 2 is an application created to allow users send message. It was created with react</p>
+                                <h6>Tech Stack:</h6>
+                                <div className="row">
+                                    <div className ="col">
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>React</p>
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>CSS3</p>
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>Node</p>
+                                    </div>
+                                    <div className ="col">
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}} >Firebase</p>
+                                    </div>
+                                </div>
                                 <button className="btn btn-light" style={{marginTop: 15}}>View Project</button>
                             </div>
                         </div>
 
                         <div className="box" style={{background: "url(https://assets.wordpress.envato-static.com/uploads/2017/02/01-diagonal-slanted-web-design.jpg) no-repeat center center", backgroundSize: "cover"}}>
+                        <div className="hover-box">
+                                <h3 className="project-box-heading">Pay Me</h3>
+                                <p>Project 2 is an application created to allow users send message. It was created with react</p>
+                                <h6>Tech Stack:</h6>
+                                <div className="row">
+                                    <div className ="col">
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>React</p>
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>CSS3</p>
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>Node</p>
+                                    </div>
+                                    <div className ="col">
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}} >Firebase</p>
+                                    </div>
+                                </div>
+                                <button className="btn btn-light" style={{marginTop: 15}}>View Project</button>
+                            </div>
                         </div>
 
                         <div className="box" style={{background: "url(https://assets.wordpress.envato-static.com/uploads/2017/02/01-diagonal-slanted-web-design.jpg) no-repeat center center", backgroundSize: "cover"}}>
+                        <div className="hover-box">
+                                <h3 className="project-box-heading">Pay Me</h3>
+                                <p>Project 2 is an application created to allow users send message. It was created with react</p>
+                                <h6>Tech Stack:</h6>
+                                <div className="row">
+                                    <div className ="col">
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>React</p>
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>CSS3</p>
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>Node</p>
+                                    </div>
+                                    <div className ="col">
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}} >Firebase</p>
+                                    </div>
+                                </div>
+                                <button className="btn btn-light" style={{marginTop: 15}}>View Project</button>
+                            </div>
                         </div>
 
                         <div className="box" style={{background: "url(https://assets.wordpress.envato-static.com/uploads/2017/02/01-diagonal-slanted-web-design.jpg) no-repeat center center", backgroundSize: "cover"}}>
+                        <div className="hover-box">
+                                <h3 className="project-box-heading">Pay Me</h3>
+                                <p>Project 2 is an application created to allow users send message. It was created with react</p>
+                                <h6>Tech Stack:</h6>
+                                <div className="row">
+                                    <div className ="col">
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>React</p>
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>CSS3</p>
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>Node</p>
+                                    </div>
+                                    <div className ="col">
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}} >Firebase</p>
+                                    </div>
+                                </div>
+                                <button className="btn btn-light" style={{marginTop: 15}}>View Project</button>
+                            </div>
                         </div>
 
                         <div className="box" style={{background: "url(https://assets.wordpress.envato-static.com/uploads/2017/02/01-diagonal-slanted-web-design.jpg) no-repeat center center", backgroundSize: "cover"}}>
+                        <div className="hover-box">
+                                <h3 className="project-box-heading">Pay Me</h3>
+                                <p>Project 2 is an application created to allow users send message. It was created with react</p>
+                                <h6>Tech Stack:</h6>
+                                <div className="row">
+                                    <div className ="col">
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>React</p>
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>CSS3</p>
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}}>Node</p>
+                                    </div>
+                                    <div className ="col">
+                                        <p style={{fontSize: ".8rem", fontWeight: 400}} >Firebase</p>
+                                    </div>
+                                </div>
+                                <button className="btn btn-light project-button">View Project</button>
+                            </div>
                         </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="contact">
+                <div className="container">
+                    <div className="contact-wrapper">
+                        <h3>Contact</h3>
+                        <div className="row">
+                            <div className ="col">
+                                <h3>Hire Me</h3>
+                                <p>If you would like to contact me for any work. Please fill out the contact form.</p>
+                            </div>
+                            <div className ="col">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Name</label>
+                                        <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email address</label>
+                                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
+                                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="exampleFormControlTextarea1">Message</label>
+                                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </section>
+
+            <section id="footer">
+                <div className="container">
+                    <div className="footer-wrapper">
+                        <p>Henry Moland @copy 2019</p>
                     </div>
                 </div>
             </section>
