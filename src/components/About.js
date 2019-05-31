8698b01f5b08f95e8d9993f3769ca68c8d2d9f3b
Henry Moland <molandh@gmail.com>
2019-05-31 13:50:07 -0700

    Change bg image

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
                     <img src={ aboutImg} alt="..." className="img-fluid about-image" />             
                     </div>
                     <div className ="col-md">
-                        <p className="about-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <p className="about-text">As a student at Lambda School, I am training to become a Full Stacks Web Developer. I aspire to create beautiful web and mobile applications that not only provide a great user experience but clean code as well. During my free time I enjoy working on personal web/mobile projects, bowling, creating music and spending time with my family.</p>
                     </div>
                 </div>
             </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,68 +1,30 @@
 import React from 'react';
 
+
 const Projects = () => {
     return (
         <section id="projects">
             <div className="project-wrapper">
                 <h3 className="section-heading">Things I have worked On</h3>
-
-                <div className="box" style={{background: "url(https://cdn-images-1.medium.com/max/1600/1*tPiV1idpK1xa05Rv_ImK-Q.png) no-repeat center center", backgroundSize: "cover"}}>
-                    <div className="hover-box">
-                        <h3 className="project-box-heading">Pay Me</h3>
-                        <p>Project 2 is an application created to allow users send message. It was created with react</p>
-                        <h6>Tech Stack:</h6>
-                        <div className="row" style={{ marginTop: 5, paddingLeft: 15}}>
-                            <div className ="col">
-                                <p className="tech-stack-text">React</p>
-                                <p className="tech-stack-text">CSS3</p>
-                                <p className="tech-stack-text">Node</p>
-                            </div>
-                            <div className ="col">
-                                <p className="tech-stack-text">Firebase</p>
-                            </div>
+                <div className="flex-container">
+                    <div className="project-image-container">
+                        <div className="box" style={{background: "url(http://i68.tinypic.com/2rxv792.png) no-repeat center center", backgroundSize: "cover"}}>    
                         </div>
-                                <button className="btn btn-light" style={{marginTop: 25}}>Preview Project</button>
+                    </div>
+                    <div className="project-text-box">
+                        <h2 className="project-box-heading">Noteful</h2>
+                        <p>Noteful is a note taking application that allow users to create notes. It is built with React on the frontend and Firebase on the backend for storing user data. Firebase authentication is also use to user authentication via the sign up/login form and Google login.</p>
+                        <h5>Tech Stack:</h5>
+                        <div className="row" style={{ marginTop: 10 }}>
+                            <div className ="col">
+                                    <p className="tech-stack-text">React, Node, Express, Materialize CSS and Firebase</p> 
+                            </div>     
+                        </div>
+                        <button className="btn btn-lg btn-light" style={{marginTop: 25}}>
+                            Preview Project
+                        </button>
                     </div>
                 </div>
-
-                <div className="box" style={{background: "url(https://www.sketchappsources.com/resources/source-image/annuacion-web-ui-kit.jpg) no-repeat center center", backgroundSize: "cover"}}>
-                    <div className="hover-box">
-                        <h3 className="project-box-heading">Pay Me</h3>
-                        <p>Project 2 is an application created to allow users send message. It was created with react</p>
-                        <h6>Tech Stack:</h6>
-                        <div className="row" style={{ marginTop: 5, paddingLeft: 15}}>
-                            <div className ="col">
-                                <p className="tech-stack-text">React</p>
-                                <p className="tech-stack-text">CSS3</p>
-                                <p className="tech-stack-text">Node</p>
-                            </div>
-                            <div className ="col">
-                                <p className="tech-stack-text">Firebase</p>
-                            </div>
-                        </div>
-                                <button className="btn btn-light" style={{marginTop: 25}}>Preview Project</button>
-                    </div>
-                </div>
-
-                <div className="box" style={{background: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa61qy73dtm_3BZfFrhPtPeZti-swL5PpZbSewTcBxeo5IR5LM) no-repeat center center", backgroundSize: "cover"}}>
-                    <div className="hover-box">
-                        <h3 className="project-box-heading">Pay Me</h3>
-                        <p>Project 2 is an application created to allow users send message. It was created with react</p>
-                        <h6>Tech Stack:</h6>
-                        <div className="row" style={{ marginTop: 5, paddingLeft: 15}}>
-                            <div className ="col">
-                                <p className="tech-stack-text">React</p>
-                                <p className="tech-stack-text">CSS3</p>
-                                <p className="tech-stack-text">Node</p>
-                            </div>
-                            <div className ="col">
-                                <p className="tech-stack-text">Firebase</p>
-                            </div>
-                        </div>
-                                <button className="btn btn-light" style={{marginTop: 25}}>Preview Project</button>
-                    </div>
-                </div>
-    
             </div>     
         </section>
     )
